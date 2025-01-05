29985376e45a8211071e3ce0b825e938211cdefc
Burri404 <burrianilkumar03@gmail.com>
2025-01-05 19:41:35 +0530

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, MainNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
